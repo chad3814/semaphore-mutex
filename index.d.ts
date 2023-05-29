a3d93081c35656df772acb8e1b64935bff315bd8
Chad Walker <chad@montra.co>
2023-05-29 16:41:44 -0500

    trying to fix types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,2 @@
-import type Semaphore from "./semaphore.d.ts";
-import type Mutex from "./mutex.d.ts";
-
-export default {Semaphore, Mutex};
+export import type Semaphore from "./semaphore.d.ts";
+export import type Mutex from "./mutex.d.ts";
